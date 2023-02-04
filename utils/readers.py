e6dbcf73261a760c49cb2331325bfd87f130ad71
Vasiko Mtsituri <vasiko.mtsituri@makingscience.com>
2023-02-04 21:35:30 +0400

    feat: add some test query and its results; Add functionality to read SQLite table as Pandas Dataframe

--- utils/readers.py
+++ utils/readers.py
@@ -1,10 +1,14 @@
+import logging
 import sqlite3
 
+import pandas as pd
 
-def get_conn_from_db_file(db: str):
+
+def get_cursor_from_db_file(db: str):
     """
     Establishes connection to the SQLite database
-    based on the path to the .db file
+    based on the path to the .db file and returns
+    cursor of that connection
 
     :param db: path to db file
     :return: sqlite3.Cursor object
@@ -27,8 +31,21 @@ def execute_query(cursor: sqlite3.Cursor, query: str):
     :return: List of tuples, representing the result
     (rows of a table) of the query execution
     """
-    res_obj = cursor.execute(query)
+    res_obj = cursor.execute(query)  # we can also use .fetchall()
 
     rows = [x for x in res_obj]
 
     return rows
+
+
+if __name__ == '__main__':
+    _conn = get_cursor_from_db_file('<path/to/db/file>')
+    # data = execute_query(conn, """Select * from camera""")
+    # data = execute_query(conn, """SELECT name FROM sqlite_master WHERE type='table';""")
+    _data = execute_query(_conn, """PRAGMA table_info('camera')""")
+
+    _conn = sqlite3.connect('../cctv_recordings/2022081205/backup.db3')
+
+    data = pd.read_sql_query("SELECT * from camera", _conn)
+
+    logging.info(data)
